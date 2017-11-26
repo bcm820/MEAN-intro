f27599cd85d3a82923196392438c7fdbeabd7949
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2017-11-25 23:13:11 -0500

    Updated fib

--- 3-Advanced/6-fib_nacci.js
+++ 3-Advanced/6-fib_nacci.js
@@ -1,24 +1,37 @@
 
+// function fib() {
+//     let x = 0;
+//     let y = 1;
+//     function nacci(){
+//         console.log(x + y);
+//         temp = y;
+//         y = x + y;
+//         x = temp;
+//     }
+//     if(x === 0){ console.log(1); }
+//     return nacci;
+// }
+
 function fib() {
-    let x = 0;
-    let y = 1;
+    let f = [0,1];
+    console.log(0); console.log(1);
     function nacci(){
-        console.log(x + y);
-        temp = y;
-        y = x + y;
-        x = temp;
+        let l = f.length;
+        console.log(f[l-1] + f[l-2]);
+        f.push(f[l-1] + f[l-2]);
     }
-    // this might be cheating :p
-    if(x === 0){ console.log(1); }
     return nacci;
 }
 
 const fibCounter = fib();
+fibCounter(); // logs first 3 numbers to accommodate for 0,1
+fibCounter(); // why does it call 'nacci' from now on??? 
 fibCounter(); 
 fibCounter(); 
 fibCounter(); 
 fibCounter(); 
-fibCounter(); 
-fibCounter(); 
+fibCounter();
+fibCounter();
+fibCounter();
 fibCounter();
 fibCounter();
